db25f642cf0ecea3421f7cadf82ad0c4952d92ea
Nick Burne <nickburne@users.noreply.github.com>
2014-11-27 08:58:35 +0000

    Update page-builder-for-canvas.php

--- page-builder-for-canvas.php
+++ page-builder-for-canvas.php
@@ -3,14 +3,14 @@
 Plugin Name: Canvas Extension - Page Builder for Canvas
 Plugin URI: http://pootlepress.com/
 Description: A page builder for Canvas.
-Version: 2.1
+Version: 2.1.1
 Author: PootlePress
 Author URI: http://pootlepress.com/
 License: GPL version 3
 */
 
 
-define('POOTLEPAGE_VERSION', '2.1');
+define('POOTLEPAGE_VERSION', '2.1.1');
 define('POOTLEPAGE_BASE_FILE', __FILE__);
 
 add_action('admin_init', 'pp_pb_check_for_conflict');
