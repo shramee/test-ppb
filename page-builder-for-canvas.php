d161d4ae7f15d46efc75a4c031d8f3d72d163cf8
Duncan <duncan@pootlepress.com>
2015-04-13 14:35:02 +0100

    Update page-builder-for-canvas.php

--- page-builder-for-canvas.php
+++ page-builder-for-canvas.php
@@ -3,14 +3,14 @@
 Plugin Name: Canvas Extension - Page Builder for Canvas
 Plugin URI: http://pootlepress.com/
 Description: A page builder for WooThemes Canvas.
-Version: 2.2.5
+Version: 2.2.6-beta.1
 Author: PootlePress
 Author URI: http://pootlepress.com/
 License: GPL version 3
 */
 
 
-define('POOTLEPAGE_VERSION', '2.2.5');
+define('POOTLEPAGE_VERSION', '2.2.6-beta.1');
 define('POOTLEPAGE_BASE_FILE', __FILE__);
 
 add_action('admin_init', 'pp_pb_check_for_conflict');
