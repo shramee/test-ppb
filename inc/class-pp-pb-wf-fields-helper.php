6573ab85bf026a8fae89eef7d710855fbe7ffeba
Shramee <shramee.srivastav@gmail.com>
2015-05-01 18:11:31 +0530

    Extracting...
    PP_PB_WF_Fields_Helper::render_field_typography

--- inc/class-pp-pb-wf-fields-helper.php
+++ inc/class-pp-pb-wf-fields-helper.php
@@ -37,40 +37,13 @@ class PP_PB_WF_Fields_Helper {
 
 		$html .= '<span class="unit-container ' . esc_attr( 'unit-' . sanitize_title_with_dashes( $unit ) ) . '">' . "\n";
 
-		/* Size in Pixels */
-		$html .= '<select class="woo-typography woo-typography-size woo-typography-size-px hide-if-em" name="'. esc_attr( $key . '[size_px]' ) . '" id="'. esc_attr( $key . '_size' ) . '">' . "\n";
-		for ( $i = 9; $i < floatval( apply_filters( 'wf_fields_typography_font_size_px_upper_limit', 71 ) ); $i++ ) {
-			$html .= '<option value="'. esc_attr( $i ) .'" ' . selected( floatval( $value['size'] ), $i, false ) . '>'. esc_html( $i ) . '</option>' . "\n";
-		}
-		$html .= '</select>' . "\n";
-
-		/* Size in EMs */
-		$html .= '<select class="woo-typography woo-typography-size woo-typography-size-em hide-if-px" name="'. esc_attr( $key . '[size_em]' ) . '" id="'. esc_attr( $key . '_size' ) . '">' . "\n";
-		$em = 0;
-		for ( $i = 0; $i < 39; $i++ ) {
-			if ( $i <= 24 )   // up to 2.0em in 0.1 increments
-				$em = $em + 0.1;
-			elseif ( $i >= 14 && $i <= 24 )  // Above 2.0em to 3.0em in 0.2 increments
-				$em = $em + 0.2;
-			elseif ( $i >= 24 )  // Above 3.0em in 0.5 increments
-				$em = $em + 0.5;
-
-			$active = '';
-			if ( strval( $em ) == $value['size'] ) {
-				$active = 'selected="selected"';
-			}
-			$html .= '<option value="' . esc_attr( floatval( $em ) ) . '" ' . $active . '>' . esc_html( $em ) . '</option>';
-		}
-		$html .= '</select>' . "\n";
+		/* Size fields */
+		$html .= $this->typography_size( $key, $value );
 
 		/* Font Unit */
-		$unit = $value['unit'];
-		$em = ''; $px = '';
-		if ( 'em' == $unit ) { $em = 'selected="selected"'; }
-		if ( 'px' == $unit ) { $px = 'selected="selected"'; }
 		$html .= '<select class="woo-typography woo-typography-unit" name="'. esc_attr( $key .'[unit]' ) . '" id="'. esc_attr( $key . '_unit' ) . '">' . "\n";
-		$html .= '<option value="px" ' . $px . '">px</option>' . "\n";
-		$html .= '<option value="em" ' . $em . '>em</option>' . "\n";
+		$html .= '<option value="px" ' . selected( $unit, 'px', false ) . '">px</option>' . "\n";
+		$html .= '<option value="em" ' . selected( $unit, 'em', false ) . '>em</option>' . "\n";
 		$html .= '</select>' . "\n";
 
 		/* Weights */
@@ -85,6 +58,63 @@ class PP_PB_WF_Fields_Helper {
 		}
 
 		/* Font Face */
+		$html .= $this->typography_font_face( $key, $value );
+
+		/* Border Color */
+		$html .= '<input id="' . esc_attr( $key . '_color' ) . '" name="' . esc_attr( $key . '[color]' ) . '" size="40" type="text" class="woo-typography-color colour" value="' . esc_attr( $value['color'] ) . '" />' . "\n";
+
+		$html .= '</span>' . "\n";
+
+		return $html;
+
+	}
+
+	/**
+	 * Outputs typography size fields
+	 *
+	 * @param $key
+	 * @param $value
+	 * @return string
+	 */
+	public function typography_size( $key, $value ){
+
+		/* Size in Pixels */
+		$html = '<select class="woo-typography woo-typography-size woo-typography-size-px hide-if-em" name="'. esc_attr( $key . '[size_px]' ) . '" id="'. esc_attr( $key . '_size' ) . '">' . "\n";
+		for ( $i = 9; $i < floatval( apply_filters( 'wf_fields_typography_font_size_px_upper_limit', 71 ) ); $i++ ) {
+			$html .= '<option value="'. esc_attr( $i ) .'" ' . selected( floatval( $value['size'] ), $i, false ) . '>'. esc_html( $i ) . '</option>' . "\n";
+		}
+		$html .= '</select>' . "\n";
+
+		/* Size in EMs */
+		$html .= '<select class="woo-typography woo-typography-size woo-typography-size-em hide-if-px" name="'. esc_attr( $key . '[size_em]' ) . '" id="'. esc_attr( $key . '_size' ) . '">' . "\n";
+		$em = 0;
+		for ( $i = 1; $i < 40; $i++ ) {
+
+			if ( $i <= 25 ){
+				$em += 0.1;// up to 2.5em in 0.1 increments
+			} else {
+				$em += 0.5;// Above 2.5em in 0.5 increments
+			}
+
+			$html .= '<option value="' . esc_attr( floatval( $em ) ) . '" ' . selected( strval( $em ), $value['size'], false ) . '>' . esc_html( $em ) . '</option>';
+		}
+		$html .= '</select>' . "\n";
+
+		return $html;
+
+	}
+
+	/**
+	 * Outputs typography font face fields
+	 *
+	 * @param $key
+	 * @param $value
+	 * @return string
+	 */
+	public function typography_font_face( $key, $value ){
+
+		$html = '';
+
 		$font_faces = wf_get_system_fonts();
 		$google_fonts = wf_get_google_fonts();
 		if ( 0 < count( $google_fonts ) ) {
@@ -114,11 +144,6 @@ class PP_PB_WF_Fields_Helper {
 			}
 			$html .= '</select>' . "\n";
 		}
-
-		/* Border Color */
-		$html .= '<input id="' . esc_attr( $key . '_color' ) . '" name="' . esc_attr( $key . '[color]' ) . '" size="40" type="text" class="woo-typography-color colour" value="' . esc_attr( $value['color'] ) . '" />' . "\n";
-
-		$html .= '</span>' . "\n";
 
 		return $html;
 
